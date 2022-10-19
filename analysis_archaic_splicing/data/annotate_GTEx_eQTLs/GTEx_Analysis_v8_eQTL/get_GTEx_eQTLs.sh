e41f5e86cb08760b230157bffda789174b917aaa
stephenrong <stephen_rong@brown.edu>
2022-10-19 09:10:13 -0400

    cleaned up data README files

--- analysis_archaic_splicing/data/annotate_GTEx_eQTLs/GTEx_Analysis_v8_eQTL/get_GTEx_eQTLs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-wget -nc https://storage.googleapis.com/gtex_analysis_v8/single_tissue_qtl_data/README_eQTL_v8.txt
-wget -nc https://storage.googleapis.com/gtex_analysis_v8/single_tissue_qtl_data/GTEx_Analysis_v8_eQTL.tar
